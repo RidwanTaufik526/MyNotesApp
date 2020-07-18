7b5db3f7e525d2ae5892c270389c673c42618b51
RidwanTaufik526 <mochridwantaufikalamsyah@gmail.com>
2020-07-18 17:03:36 +0700

    ConsumerApp Module Added To MyNotesApp

---
+++ consumerapp/src/main/java/com/ridwantaufik/apps/consumerapp/db/DatabaseContract.kt
@@ -0,0 +1,26 @@
+package com.ridwantaufik.apps.consumerapp.db
+
+import android.net.Uri
+import android.provider.BaseColumns
+
+object DatabaseContract {
+
+    const val AUTHORITY = "com.ridwantaufik.apps.mynotesapp"
+    const val SCHEME = "content"
+
+    class NoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_NAME = "note"
+            @Suppress("ObjectPropertyName")
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val DATE = "date"
+
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
+        }
+    }
+}
